5f26e3f393b3ee3c39ba058c5a8d9da5375bdd1f
Oded Arlevski <arlevski@gmail.com>
2018-12-30 18:52:01 +0200

    Initial Commit

---
+++ entranceSystem/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  entranceSystem
+//
+//  Created by Oded Arlevski on 30/12/2018.
+//  Copyright © 2018 Oded Arlevski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
