d98196616abca8b201931f61745ef5f3a06f585a
Oded Arlevski <arlevski@gmail.com>
2018-12-31 13:52:16 +0200

    Source.c code merged with main.c

--- entranceSystem/main.c
+++ entranceSystem/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+#define ACCESS_PATH "/Users/odedarlevski/Documents/Projects/entranceSystem/entranceSystem/access.txt"
+/***
+ * This is the path where the access file is located.
+ * When you submit the program make sure this path match to: "c:\\temp\\access.txt"
+ ***/
+
+#define LOG_PATH "log.txt"
+/***
+ * This is the path where the log file is located.
+ * Open this file and append new records into it.
+ * When you submit the program make sure this path match to: "c:\\temp\\log.txt"
+ ***/
 
 
 typedef struct S_User
@@ -48,9 +63,61 @@ User *AddUser(User *previous) {
     return newUser;
 }
 
-//void userCreate();
+void readAccess(char *path);
+/***
+ * Handle function: get's the path of the access file, and print all users information.
+ * You may change it according to your needs.
+ ***/
 
-//void readAccess();
+void getDateTime(int *day, int *month, int *year, int *hours, int *mins);
+/***
+ * Handle function: Returns by referfance the current date and time
+ ***/
+
+
+
+void readAccess(char *path)
+{
+    FILE *fp;
+    int status, pulse;
+    char temp[100], name[21], code[9], date_s[11], date_e[11], time_s[6], time_e[6];
+    
+    fp = fopen( path, "r");
+    if (!fp)
+    {
+        printf("File not found!\n");
+        return;
+    }
+    
+    //header
+    fgets(temp, 100, fp);
+    puts(temp);
+    
+    while (fscanf(fp, "%20s %8s %1d %10s %10s %5s %5s %5d", name, code, &status, date_s, date_e, time_s, time_e, &pulse) != EOF)
+    {
+        printf("%-20s %-8s %-1d %-10s %-10s %-5s %-5s %-5d\n", name, code, status, date_s, date_e, time_s, time_e, pulse);
+    }
+    
+    fclose(fp);
+    return;
+}
+
+
+void getDateTime(int *day, int *month, int *year, int *hours, int *mins)
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    
+    time(&rawtime);
+    timeinfo = localtime( &rawtime);
+    
+    *day = timeinfo->tm_mday;
+    *month = timeinfo->tm_mon + 1;
+    *year = timeinfo->tm_year + 1900;
+    *hours = timeinfo->tm_hour;
+    *mins = timeinfo->tm_min;
+}
+
 
 
 
@@ -60,6 +127,7 @@ User *AddUser(User *previous) {
 
 int main(int argc, char *argv[]) {
     
+    readAccess(ACCESS_PATH);
 
     char command[16];
     char input[16];
